3f5cfdabb627b62a86dde4a6c0af0fe8ddbad910
User <User@192.168.123.106>
2020-04-05 20:12:05 +0900

    2020년 4월5일자 자료구조 3주차 과제

---
+++ 자료구조 과제/src/과제/main4.java
@@ -0,0 +1,24 @@
+package 과제;
+
+public class main4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CList<String> s = new CList<String>();
+		
+		s.insert("pear");
+		s.insert("cherry");
+		s.insert("orange");
+		s.insert("apple");
+		s.print(); 
+		
+		System.out.print(" : s의 길이 = " + s.size() + "\n");
+		
+		s.delete();
+		s.print();
+		System.out.print(" : s의 길이 = " + s.size());
+		System.out.println();
+		
+	}
+
+}
